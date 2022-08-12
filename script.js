5b483826154f36cf2245ad4800c31e387fdab4b3
Adib <mansuri.adib25@gmail.com>
2022-08-12 23:40:07 +0200

    challange 1.2

---
+++ script.js
@@ -0,0 +1,43 @@
+// "use strict";
+// function logger() {
+//   console.log("i am adib");
+// }
+// logger();
+
+// challange 1
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(2, 5, 5));
+
+const scoreDolphins = calcAverage(44, 23, 71);
+const scorekoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scorekoalas);
+
+const checkwinner = function (avgdolphins, avgkoalas) {
+  if (avgdolphins >= 2 * avgkoalas) {
+    console.log(`Dolphins win`);
+  } else if (avgkoalas >= 2 * avgdolphins) {
+    console.log(`koalas win`);
+  } else {
+    console.log(`No team wins`);
+  }
+};
+checkwinner(scoreDolphins, scorekoalas);
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(2, 5, 5));
+
+const scoreDolphins = calcAverage(44, 23, 71);
+const scorekoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scorekoalas);
+
+const checkwinner = function (avgdolphins, avgkoalas) {
+  if (avgdolphins >= 2 * avgkoalas) {
+    console.log(`Dolphins win`);
+  } else if (avgkoalas >= 2 * avgdolphins) {
+    console.log(`koalas win`);
+  } else {
+    console.log(`No team wins`);
+  }
+};
+checkwinner(scoreDolphins, scorekoalas);
