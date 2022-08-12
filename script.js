7bae58ac3cea7eaef0b27d7ee909be20726cbf38
Adib <mansuri.adib25@gmail.com>
2022-08-13 00:41:20 +0200

    js lern arrays

--- script.js
+++ script.js
@@ -4,22 +4,26 @@
 // }
 // logger();
 
-// challange 1
+// // challange 1
 
-const calcAverage = (a, b, c) => (a + b + c) / 3;
-console.log(calcAverage(2, 5, 5));
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
+// console.log(calcAverage(2, 5, 5));
 
-const scoreDolphins = calcAverage(44, 23, 71);
-const scorekoalas = calcAverage(65, 54, 49);
-console.log(scoreDolphins, scorekoalas);
+// const scoreDolphins = calcAverage(44, 23, 71);
+// const scorekoalas = calcAverage(65, 54, 49);
+// console.log(scoreDolphins, scorekoalas);
 
-const checkwinner = function (avgdolphins, avgkoalas) {
-  if (avgdolphins >= 2 * avgkoalas) {
-    console.log(`Dolphins win`);
-  } else if (avgkoalas >= 2 * avgdolphins) {
-    console.log(`koalas win`);
-  } else {
-    console.log(`No team wins`);
-  }
-};
-checkwinner(scoreDolphins, scorekoalas);
+// const checkwinner = function (avgdolphins, avgkoalas) {
+//   if (avgdolphins >= 2 * avgkoalas) {
+//     console.log(`Dolphins win`);
+//   } else if (avgkoalas >= 2 * avgdolphins) {
+//     console.log(`koalas win`);
+//   } else {
+//     console.log(`No team wins`);
+//   }
+// };
+// checkwinner(scoreDolphins, scorekoalas);
+
+//arrays
+const adib = ["adib", "mansuri", "23"];
+console.log(adib);
