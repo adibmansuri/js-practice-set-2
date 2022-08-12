d98807db427e1ee20decdf6fcb16d21873509ecc
Adib <mansuri.adib25@gmail.com>
2022-08-13 01:54:02 +0200

    js lern challange 2

--- script.js
+++ script.js
@@ -32,3 +32,10 @@ adib.push("happy");
 console.log(adib);
 
 // exercise
+const clicktip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.5 : bill * 0.2;
+};
+const bills = [125, 555, 44];
+const tips = [clicktip(bills[0]), clicktip(bills[1]), clicktip(bills[2])];
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(tips, bills, total);
