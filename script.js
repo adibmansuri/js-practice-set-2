22ca8788577be147f1837d45e4da71f7d98dd2e5
Adib <mansuri.adib25@gmail.com>
2022-08-14 00:51:00 +0200

    challange complited

--- script.js
+++ script.js
@@ -32,25 +32,73 @@
 // console.log(adib);
 
 // // exercise
-// const clicktip = function (bill) {
-//   return bill >= 50 && bill <= 300 ? bill * 0.5 : bill * 0.2;
-// };
-// const bills = [125, 555, 44];
-// const tips = [clicktip(bills[0]), clicktip(bills[1]), clicktip(bills[2])];
-// const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
-// console.log(tips, bills, total);
+// // const clicktip = function (bill) {
+// //   return bill >= 50 && bill <= 300 ? bill * 0.5 : bill * 0.2;
+// // };
+// // const bills = [125, 555, 44];
+// // const tips = [clicktip(bills[0]), clicktip(bills[1]), clicktip(bills[2])];
+// // const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// // console.log(tips, bills, total);
 
-// object
-const adib = {
-  firstname: "Adib",
-  lastname: "Mansuri",
-  friends: ["michael", "peter", "steven"],
-};
-adib.location = "india";
-adib["job"] = "teacher";
-console.log(adib);
+// // object
+// const adib = {
+//   firstname: "Adib",
+//   lastname: "Mansuri",
+//   friends: ["michael", "peter", "steven"],
+//   birthyear: 1996,
+//   hasdriverlicense: true,
+//   calcage: function () {
+//     this.age = 2022 - this.birthyear;
+//     return this.age;
+//   },
+// };
+// adib.location = "india";
+// adib["job"] = "teacher";
+// // console.log(job);
+// console.log(adib.calcage);
+// console.log(adib);
+// // challange
+// console.log(adib.age);
+// // console.log(
+// //   `${adib.firstname}  has ${adib.friends.length} friends, and his bestfriend is ${adib.friends[0]}`
+// // );
+
+// console.log(
+// hasdriverlicense == true{
+//   ` ${adib.firstname} is ${adib.calcage}-year old ${adib.job} and he has a driving license.
+// `}
+// );
+
 // challange
 
-console.log(
-  `${adib.firstname}  has ${adib.friends.length} friends, and his bestfriend is ${adib.friends[0]}`
-);
+const mark = {
+  fullname: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcbmi: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+const johan = {
+  fullname: "Johan Smith",
+  mass: 92,
+  height: 1.95,
+  calcbmi: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+mark.calcbmi();
+johan.calcbmi();
+console.log(mark.bmi);
+console.log(johan.bmi);
+if (mark.bmi > johan.bmi) {
+  console.log(
+    `${mark.fullname}'s BMI ${mark.bmi} is higher than ${johan.fullname}'s BMI ${johan.bmi}`
+  );
+} else if (johan.bmi > mark.bmi) {
+  console.log(
+    `${johan.fullname}'s BMI ${johan.bmi} is higher than ${mark.fullname}'s BMI ${mark.bmi}`
+  );
+}
