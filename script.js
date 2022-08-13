a7d20af08d14074f900192034d9de0b26889fb4f
Adib <mansuri.adib25@gmail.com>
2022-08-13 03:04:09 +0200

    js lern object

--- script.js
+++ script.js
@@ -25,17 +25,27 @@
 // checkwinner(scoreDolphins, scorekoalas);
 
 //arrays
-const adib = ["adib", "mansuri", "23"];
-console.log(adib);
-// add element
-adib.push("happy");
-console.log(adib);
+// const adib = ["adib", "mansuri", "23"];
+// console.log(adib);
+// // add element
+// adib.push("happy");
+// console.log(adib);
 
-// exercise
-const clicktip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.5 : bill * 0.2;
+// // exercise
+// const clicktip = function (bill) {
+//   return bill >= 50 && bill <= 300 ? bill * 0.5 : bill * 0.2;
+// };
+// const bills = [125, 555, 44];
+// const tips = [clicktip(bills[0]), clicktip(bills[1]), clicktip(bills[2])];
+// const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// console.log(tips, bills, total);
+
+// object
+const adib = {
+  firstname: "adib",
+  lastname: "mansuri",
+  friends: ["michael", "peter", "steven"],
 };
-const bills = [125, 555, 44];
-const tips = [clicktip(bills[0]), clicktip(bills[1]), clicktip(bills[2])];
-const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
-console.log(tips, bills, total);
+adib.location = "india";
+adib["job"] = "teacher";
+console.log(adib);
